d5f6dc1886bf708fac265e8c85a93e15272ef69a
PandaBlockBoy <arthuralmeida.office.dev@gmail.com>
2023-02-21 23:25:23 -0300

    signup verification page created

---
+++ public/scripts/customer/signup-verification.js
@@ -0,0 +1,36 @@
+const inputElements = [...document.querySelectorAll(".code-input")];
+const form = document.querySelector(".form");
+
+inputElements.forEach((ele, index) => {
+    ele.addEventListener("keydown", (e) => {
+        if(e.keyCode === 8 && e.target.value === "") {
+            inputElements[Math.max(0, index - 1)].focus();
+        };
+    });
+    ele.addEventListener("input", (e) => {
+        const [first, ...rest] = e.target.value;
+        const lastInputBox = index === inputElements.length-1;
+        const didInsertContent = first !== undefined;
+
+        e.target.value = first ?? ""
+        if (didInsertContent && !lastInputBox) {
+            inputElements[index+1].focus();
+            inputElements[index+1].value = rest.join("");
+            inputElements[index+1].dispatchEvent(new Event("input"));
+        };
+
+        allFIlled();
+    });
+});
+
+function allFIlled() {
+    var filled = 0;
+    inputElements.forEach((ele, index) => {
+        if (inputElements[index].value !== "") {
+            filled++
+        };
+    });
+    if (filled == 6) {
+        form.submit()
+    };
+};
