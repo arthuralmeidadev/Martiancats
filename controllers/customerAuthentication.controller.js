a59f67f3ea18cfa9f516f81b59cbcec3de35d610
PandaBlockBoy <arthuralmeida.office.dev@gmail.com>
2023-02-21 23:21:26 -0300

    add customer auth controll

---
+++ controllers/customerAuthentication.controller.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+const googleAuthenticationData = require("../data/googleAuthenticationData.json");
+
+async function sendVerificationEmail(req, res) {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: "arthuralmeida.office.dev@gmail.com",
+            pass: "Art19-Gvt11",
+            clientId: googleAuthenticationData.clientId,
+            clientSecret: googleAuthenticationData.clientSecret,
+            refreshToken: googleAuthenticationData.refresh_token
+        }
+    });
+
+    const mailOptions = {
+        from: "arthuralmeida.office.dev@gmail.com",
+        to: "meupanda374@gmail.com",
+        subject: "Nodemailer Project",
+        text: "Hi from your nodemailer project",
+        priority: "high"
+    };
+
+    /*transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+          console.log("Error " + err);
+        } else {
+          console.log("Email sent successfully");
+        };
+    });*/
+    
+    return res.render("../views/customer/my-account/signup-verification");
+};
+
+async function verifyCode(req, res) {
+    const { code } = req.body;
+    return res.send(code.join(""))
+};
+
+module.exports = {
+    sendVerificationEmail,
+    verifyCode
+};
