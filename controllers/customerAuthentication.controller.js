7c1d2985722763ce3134d9e1a1b180b1ba779896
PandaBlockBoy <arthuralmeida.office.dev@gmail.com>
2023-02-23 01:36:26 -0300

    beautification

--- controllers/customerAuthentication.controller.js
+++ controllers/customerAuthentication.controller.js
@@ -2,49 +2,47 @@ const fs = require("fs-extra");
 const nodemailer = require("nodemailer");
 const gmailCredentials = require("../data/gmailCredentials.json");
 const generateValidationCode = require("../helpers/generateValidationCode");
+const cachePath = "./cache/emailValidationCodes.json";
+
 
 async function sendVerificationEmail(req, res) {
     const { signupIssuer, signupFirstName, signupLastName } = req.body;
     
-    const filePath = "./cache/emailValidationCodes.json";
-    const validationCodes = fs.readJsonSync(filePath);
+    
+    const validationCodes = fs.readJsonSync(cachePath);
     const code = await generateValidationCode(validationCodes.map(entry => entry.code));
     const expiry = Math.floor(Date.now() / 1000) + 120; // 2 minutes from now
 
+    const mailOptions = {
+        from: gmailCredentials.provider,
+        to: signupIssuer,
+        subject: "Verification Code - Martiancats Accounts",
+        text: `Hello ${signupFirstName} ${signupLastName} This is your verification code: ${code}`,
+        priority: "high"
+    };
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: gmailCredentials.provider,
+            pass: gmailCredentials.secret,
+            clientId: gmailCredentials.clientId,
+            clientSecret: gmailCredentials.clientSecret,
+            refreshToken: gmailCredentials.refresh_token
+        }
+    });
+
     validationCodes.push({ code: code, expiry: expiry });
 
-    fs.writeJson(filePath, validationCodes, (err) => {
+    await fs.writeJson(cachePath, validationCodes, { spaces: 2});
+
+    transporter.sendMail(mailOptions, (err, email) => {
         if (err) {
             return res.sendStatus(500);
         };
 
-        const mailOptions = {
-            from: gmailCredentials.provider,
-            to: signupIssuer,
-            subject: "Verification Code - Martiancats Accounts",
-            text: `Hello ${signupFirstName} ${signupLastName} This is your verification code: ${code}`,
-            priority: "high"
-        };
-
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                type: "OAuth2",
-                user: gmailCredentials.provider,
-                pass: gmailCredentials.secret,
-                clientId: gmailCredentials.clientId,
-                clientSecret: gmailCredentials.clientSecret,
-                refreshToken: gmailCredentials.refresh_token
-            }
-        });
-
-        transporter.sendMail(mailOptions, (err, email) => {
-            if (err) {
-                return res.sendStatus(500);
-            };
-
-            return res.render("../views/customer/my-account/signup-verification");
-        });
+        return res.render("../views/customer/my-account/signup-verification");
     });
 };
 
@@ -53,22 +51,16 @@ async function validateCode(req, res) {
 
     const parsedCode = code.join("");
 
-    const filePath = "./cache/emailValidationCodes.json";
-
     const validationCodes = fs.readJsonSync(filePath);
-    
+
     const updatedFile = validationCodes.filter(
         entry => entry.code !== parsedCode &&
         entry.expiry >= Date.now() / 1000
     );
 
-    fs.writeJson("./cache/emailValidationCodes.json", updatedFile, (err) => {
-        if (err) {
-            return res.sendStatus(500);
-        };
-
-        return res.json({ message: "Welcome to your new account" });
-    });
+    await fs.writeJson(cachePath, updatedFile, { spaces: 2});
+    
+    return res.json({ message: "Welcome to your new account" });
 };
 
 module.exports = {
