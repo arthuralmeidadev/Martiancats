ef1580c10f7ed672368161c323a47a02eb04d630
PandaBlockBoy <arthuralmeida.office.dev@gmail.com>
2023-02-28 18:40:01 -0300

    feat/authentication middleware functionality

--- middlewares/customerAuthentication.js
+++ middlewares/customerAuthentication.js
@@ -1,4 +1,16 @@
+const tokenizer = require("../helpers/tokenizer");
+
 async function customerAuthenticationMiddleware (req, res, next) {
+    const { accessToken } = req.cookies;
+    if (!accessToken) {
+        return res.status(401);
+    } else {
+        try {
+            await tokenizer.verifyAccessToken(accessToken);
+        } catch (err) {
+            return res.redirect("/refresh")
+        };
+    };
 
     next();
 };
