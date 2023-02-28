bc046d52bccc92a86926d3e8ef840ced3cd3a713
PandaBlockBoy <arthuralmeida.office.dev@gmail.com>
2023-02-28 18:38:37 -0300

    fix/authentication middleware not redirecting

--- middlewares/adminAuthentication.js
+++ middlewares/adminAuthentication.js
@@ -5,6 +5,12 @@ async function adminAuthenticationMiddleware(req, res, next) {
     const { accessToken } = req.cookies;
     if (!accessToken) {
         return res.status(401);
+    } else {
+        try {
+            await tokenizer.verifyAccessToken(accessToken);
+        } catch (err) {
+            return res.redirect("admin/refresh")
+        };
     };
     const decoded = await tokenizer.verifyAccessToken(accessToken);
     const payload = await encrypter.decrypt(decoded);
